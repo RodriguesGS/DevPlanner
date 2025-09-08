078cd9d7e54fd362a00e54dddc5616bf228d48d3
Gabriel <rodriguesgasoares@gmail.com>
2025-09-08 14:45:39 -0300

    feat: add tela de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './features/auth/ui/login-page/login-page.component';
+import { RegisterPageComponent } from './features/auth/ui/register-page/register-page.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full'},
-    { path: 'login', component: LoginPageComponent, title: "Dev Planner | Login" }
+    { path: 'login', component: LoginPageComponent, title: "Dev Planner | Login" },
+    { path: 'register', component: RegisterPageComponent, title: "Dev Planner | Registre-se" }
 ];
--- src/app/features/auth/ui/login-page/login-page.component.ts
+++ src/app/features/auth/ui/login-page/login-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon'
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
   imports: [
-    MatIconModule
-  ],
+    MatIconModule,
+    RouterLink
+],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })

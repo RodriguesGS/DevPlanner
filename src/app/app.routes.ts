0df45eb7545c75738aa736fcacb4c831899539e0
Gabriel <rodriguesgasoares@gmail.com>
2025-09-08 12:01:04 -0300

    feat: Configurações iniciais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './features/auth/ui/login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full'},
+    { path: 'login', component: LoginPageComponent, title: "Dev Planner | Login" }
+];
---
+++ src/app/features/auth/ui/login-page/login-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon'
+
+@Component({
+  selector: 'app-login-page',
+  imports: [
+    MatIconModule
+  ],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss'
+})
+export class LoginPageComponent {
+  
+}

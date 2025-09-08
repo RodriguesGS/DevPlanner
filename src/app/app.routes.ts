86b66571b39a5dd39bdce591d3e30c20b69ca496
Gabriel <rodriguesgasoares@gmail.com>
2025-09-08 17:15:32 -0300

    feat: add tela de recover de senha

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,20 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './features/auth/ui/login-page/login-page.component';
-import { RegisterPageComponent } from './features/auth/ui/register-page/register-page.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full'},
     { path: 'login', component: LoginPageComponent, title: "Dev Planner | Login" },
-    { path: 'register', component: RegisterPageComponent, title: "Dev Planner | Registre-se" }
+
+    { 
+        path: 'register', 
+        loadComponent: () => import('./features/auth/ui/register-page/register-page.component')
+            .then((m) => m.RegisterPageComponent), 
+        title: "Dev Planner | Registre-se"
+    },
+
+    { 
+        path: 'recover-password',  
+        loadComponent: () => import('./features/auth/ui/forgot-password/forgot-password.component')
+            .then((m) => m.ForgotPasswordComponent), 
+        title: "Dev Planner | Recuperar senha" }
 ];
--- src/app/features/auth/ui/forgot-password/forgot-password.component.ts
+++ src/app/features/auth/ui/forgot-password/forgot-password.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  imports: [
+    MatIconModule,
+    RouterLink
+],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss'
 })

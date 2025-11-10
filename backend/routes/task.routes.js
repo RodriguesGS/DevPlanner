a7a9c868217a6c4e677bee125367659de3a109c0
nathan.pulchinelli <nathan.pulchinelli@tecnospeed.com.br>
2025-11-10 14:45:09 -0300

    solução de erro de eslint

--- backend/routes/task.routes.js
+++ backend/routes/task.routes.js
@@ -10,22 +10,16 @@ import {
 
 const router = express.Router();
 
-// GET all tasks
 router.get('/', getAllTasks);
 
-// GET tasks by project
 router.get('/project/:projectId', getTasksByProject);
 
-// GET task by ID
 router.get('/:id', getTaskById);
 
-// POST create new task
 router.post('/', createTask);
 
-// PUT update task
 router.put('/:id', updateTask);
 
-// DELETE task
 router.delete('/:id', deleteTask);
 
 export default router;
